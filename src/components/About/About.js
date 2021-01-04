8b1b54b8e236f1926e298c8908d4ea196abfc260
Elvin Samanov <samanov.elvin96@mail.ru>
2021-01-04 19:35:29 +0400

    localization about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react'
 import Sli from "../Slider/Slider";
 import { Breadcrumb, BreadcrumbItem } from "../Breadcrumbs/Breadcrumbs";
 
-
 import "./About.css";
-export default class Info extends Component {
-    render() {
+
+import { useTranslation } from 'react-i18next';
+
+const About=()=> {
+
+    const { t, i18n } = useTranslation(['about']);
+
         return (
         
 <>
-<Breadcrumb title="Naa haqqında">
-        <BreadcrumbItem name="Ana səhifə" href="/" />
-        <BreadcrumbItem name="Naa haqqında" current />
+<Breadcrumb title={t('title')}>
+        <BreadcrumbItem name={t('home')} href="/" />
+        <BreadcrumbItem name={t('title')} current />
 </Breadcrumb>
 <div className="container">
 <div className="row">
     <div className="col-12 col-md-6">
     <p>
-    Milli Aviasiya Akademiyası (MAA) — Azərbaycan Respublikasının dövlət ali təhsil müəssisəsidir və Azərbaycan Hava Yolları Qapalı Səhmdar Cəmiyyətinin (QSC) strukturuna daxildir. Milli Aviasiya Akademiyası Azərbaycanda mülki aviasiya sahəsində ali, əlavə və peşə-ixtisas təhsil proqramlarını həyata keçirən ali təhsil müəssisəsidir.
-<br/> <br/>    Azərbaycan Respublikası mülki aviasiya sahəsində milli kadrlara olan ehtiyacın ödənilməsi məqsədilə Azərbaycan Respublikası Nazirlər Kabinetinin 15 iyun 1992-ci il tarixli 337 saylı qərarı ilə «Azərbaycan Hava Yolları» Dövlət Konserni (indiki «Azərbaycan Hava Yolları» Qapalı Səhmdar Cəmiyyəti) nəzdində Milli Aviasiya Mərkəzi yaradıldı
-
-Mərkəzin nəzdində Ümumi texniki fənlər və Humanitar fənlər kafedraları təşkil olundu. 1993-cü ildə Mərkəzdə Hava Nəqliyyatının İstismarı fakültəsi və Aviasiya xüsusi avadanlığı və İngilis dili kafedraları yaradıldı.
-
-Azərbaycan Respublikası Nazirlər Kabinetinin 24 fevral 1994-cü il tarixli 81 saylı qərarı ilə Milli Aviasiya Mərkəzi Milli Aviasiya Akademiyası adlandırıldı.</p>
+    {t('content1')}
+<br/> <br/>  {t('content2')}</p>
     </div>
     <div className="col-12 col-md-6">
     <img src="https://aviateka.su/wp-content/uploads/2018/03/naa-az-1024x709.jpg" alt="photo-1" width="100%" />
@@ -31,32 +31,9 @@ Azərbaycan Respublikası Nazirlər Kabinetinin 24 fevral 1994-cü il tarixli 81
 <div className="row">
     <div className="col-12">
 <p>
-Milli Aviasiya Akademiyası bir fakültə, iki ixtisas, 80 tələbə ilə fəaliyyətə başladı. 1994-cü ildə Hava Nəqliyyatının Uçuş İstismarı fakültəsi və onun nəzdində Hava gəmilərinin uçuş istismarı və aeronaviqasiya, Uçuş aparatlarının və aviasiya mühərriklərinin konstruksiyası və istismarı kafedraları yaradıldı. Həmin ildə MAA-nın nəzdində Elmi Tədqiqat Aviasiya institutu (ETAİ) yaradıldı.
-1996-cı ildə Milli Aviasiya Akademiyasının rektoru vəzifəsinə Arif Mir Cəlal oğlu Paşayev təyin olundu.
-
-Azərbaycanda mülki aviasiyanın inkişafı ilə əlaqədar olaraq 1997-1998-ci tədris ilindən MAA-ya qəbul olan tələbələrin sayı artmağa başladı. MAA-nın məzunlarının ilk buraxılışı 1995/1996-cı tədris ilində oldu. Həmin ildə 39 tələbə MAA-nın məzunu oldu.
-
-1999-cu ildə Avianəqliyyat istehsalatı fakültəsi, onun nəzdində Avianəqliyyat istehsalatı, Riyaziyyat və mexanika kafedraları, İqtisadiyyat və hüquq fakültəsi və onun nəzdində Hüquqşünaslıq kafedrası yaradıldı. Həmin dövrdə Milli Aviasiya Akademiyasının «Elmi məcmuələr» jurnalı təsis olundu və Azərbaycan Respublikası Prezidenti yanında Ali Attestasiya Komissiyasının reyestrinə daxil edildi.
-
-Tədris prosesinin beynəlxalq aviasiya təşkilatlarının tələblərinə uyğun olaraq qurulması məqsədilə 1999-cu ildən etibarən MAA Dövlətlərarası Aviasiya Komitəsi (DAK) tərəfindən sertifikarlaşdırılmış və dövri olaraq onun qüvvəsi yenilənmişdir.
-
-DAK tərəfindən aparılmış sertifikasiyanın nəticəsi olaraq Akademiyanın məzunlarının diplomları DAK üzvü olan ölkələrdə tanınmağa başlamışdır.
-MAA 2000-ci ildə ICAO-nun Sorğu kitabına, 2002-ci ildə isə İCAO-nun reyestrinə daxil edilmişdir.
+{t('content3')}
 <br/>
-2005-ci ildə MAA-da “Aviasiya tarixi muzeyi” yaradıldı.
-<br/>
-Həmin ildə Milli Aviasiya Akademiyası DAK-ın qərarı ilə Mülki Aviasiya Ali Təhsil Müəssisəsi Sertifikatına (Şəhadətnaməsinə) layiq görüldü.
-Azərbaycan Respublikasının Dövlət Mülki Aviasiya Administrasiyası (DMAA) tərəfindən qəbul olunmuş qaydalara və prosedurlara, Beynəlxalq Mülki Aviasiya təşkilatları tərəfindən qəbul edilmiş qaydalara, standartlara, tövsiyə edilən təcrübə və prosedurlara, digər beynəlxalq normativ-hüquqi aktların tələblərinə uyğunluğunun müəyyən edilməsi məqsədi ilə Milli Aviasiya Akademiyası 2006-cı ildə Azərbaycan Respublikasının Təhsil Nazirliyinin akkreditasiyasından keçmiş və ona ali məktəb statusunu müəyyən edən 001 saylı sertifikat verilmişdir.
-<br/>
-2009-cu ildə MAA nəzdində olan ETAİ-nin adı dəyişdirilərək Elmi-Tədqiqat Nəqliyyat və Aviakosmik Problemləri İnstitutu (ETNAPİ) adlandırıldı.
-<br/>
-2010-cu ilin oktyabr ayında MAA-da pilotların məqsədyönlü, davamlı və fasiləsiz, ilkin və yenidən hazırlığı, həmçinin uçuş heyəti üzvlərinin istehsalatdan ayrılmadan ixtisaslarını artırmaları məqsədi ilə Trenajor mərkəzi istifadəyə verildi.
-<br/>
-2010-2011-ci tədris ilində MAA-da magistratura səviyyəsinə ilk dəfə qəbul aparıldı.
-<br/>
-2011-ci ilin dekabr ayında Milli Aviasiya Akademiyasının DAK və DMAA tərəfindən növbəti sertifikasiyaları keçirildi və MAA-ya verilmiş sertifikatların müddəti 2014-cü ilə qədər artırıldı.
-<br/>
-2012-ci ildə MAA Təhsil Nazirliyinin növbəti akkreditasiyasından keçdi və 089 saylı sertifikat verildi. Həmin ilin yanvar ayında MAA Avropa Mülki Aviasiya Konfransının (ECAC) təlim mərkəzləri şəbəkəsinə üzv qəbul edildi və bununla Akademiyada mütəxəssis hazırlığının beynəlxalq standartların tələblərinə uyğunluğu təsdiq edildi.
+{t('content4')}
 <br/>
 </p>
 </div>
@@ -67,32 +44,24 @@ Azərbaycan Respublikasının Dövlət Mülki Aviasiya Administrasiyası (DMAA) 
 
 <div className="col-12">
 <p>
-2012-ci il may ayının 30-da Milli Aviasiya Akademiyası “Təhlükəli yüklərin hava nəqliyyatı ilə daşınması” üzrə Beynəlxalq Hava Nəqliyyatı Assosiasiyasının (İATA) regional tədris mərkəzi statusunu aldı.
-<br/>
-2014-cü ilin sentyabr ayında MAA-da 2 yataqxana korpusu, futbol meydançası, tennis kortu, voleybol və basketbol meydançalarına malik tələbə şəhərciyi istifadəyə verildi.
-<br/>
-2016-cı ildə Azərbaycanda ilk dəfə olaraq xanım pilotların hazırlığına başlanıldı.
-<br/>
-2018-ci ildə MAA Azərbaycan Respublikasının Təhsil Nazirliyi tərəfindən növbəti 5 il müddətinə akkreditasiya olundu. Həmin ildə MAA həmçinin Dövlətlərarası Aviasiya Komitəsi və Azərbaycan Respublikasının Nəqliyyat, Rabitə və Yüksək Texnologiyalar Nazirliyi yanında Dövlət Mülki Aviasiya Agentliyi tərəfindən sertifikasiyadan keçdi.
-<br/>
-2019-cu ildə Milli Aviasiya Akademiyası DMAA tərəfindən Azərbaycan Aviasiya Qaydaları (AAQ) "Part 147 Subpart C"-nin tələblərinə uyğun Texniki xidmət üzrə təlim və imtahan təşkilatı kimi sertifikasiyadan keçdi və həmin ilin oktyabr ayından MAA-da “Hava gəmilərinə texniki xidmət göstərən personalın hazırlığı üzrə Texnik-mexanik (B-1) və Texnik-avionik (B-2) kursları”na qəbul həyata keçirildi.
+{t('content5')}
 </p>
 <br/>
 <div className="col-12" style={{textAlign:"center",marginBottom:30}}>
 <h2>
-Rəhbərlik
+{t('boss')}
 </h2>
 </div>
 <br/>
 <div className="col-12" style={{marginBottom:30}}>
 
     <ul>
-        <li>Arif Paşayev-rektor</li>
-        <li>Ədalət Səmədov-tədris işləri üzrə prorektor</li>
-        <li>Afiq Həsənov-Elmi işlər üzrə prorektor</li>
-        <li>Hacıağa Əliyev-uçuş işləri üzrə prorektor</li>
-        <li>Şəmsəddin Axundov-ictimaiyyətlə əlaqə üzrə prorektor</li>
-        <li>Rafiq Həsənov-Ümumi və təsərrüfat-texniki məsələlər üzrə prorektor</li>
+        <li>{t('boss1')}</li>
+        <li>{t('boss2')}</li>
+        <li>{t('boss3')}</li>
+        <li>{t('boss4')}</li>
+        <li>{t('boss5')}</li>
+        <li>{t('boss6')}</li>
 
     </ul>
 
@@ -105,5 +74,5 @@ Rəhbərlik
 </>
         
         )
-    }
 }
+export default About;
