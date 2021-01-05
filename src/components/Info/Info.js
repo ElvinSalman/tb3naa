150877a82af64b6bb244dbf4d6be6d67e1d18734
Elvin Samanov <samanov.elvin96@mail.ru>
2021-01-05 11:50:17 +0400

    local for info

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem } from "../Breadcrumbs/Breadcrumbs";
 
+import { useTranslation } from 'react-i18next';
+
 import tb from "../../data/tb3Info.json"
 
-export default class Info extends Component {
-    render() {
+const Info=()=> {
+
+    const { t, i18n } = useTranslation(['']);
+    const lang=i18n.language;
+    let title=lang=="en"?"TB3-117BM Engine Subtopics":lang=="ru"?"Подтемы двигателя TB3-117M":"TB3-117BM mühərrikinə aid altmövzular";
+    let home=lang=="en"?"Home":lang=="ru"?"Главная":"Ana səhifə";
+
         return (
         
 <>
-<Breadcrumb title="TB3-117BM mühərrikinə aid altmövzular">
-        <BreadcrumbItem name="Ana səhifə" href="/" />
-        <BreadcrumbItem name="TB3-117BM mühərrikinə aid altmövzular" current />
+<Breadcrumb title={title}>
+        <BreadcrumbItem name={home} href="/" />
+        <BreadcrumbItem name={title} current />
 </Breadcrumb>
 <div className="container list-group" style={{marginTop:50,marginBottom:50}}>
 
@@ -19,7 +26,7 @@ export default class Info extends Component {
   <div className="row d-flex justify-content-center">
             
             <div className="col-10 col-md-6">
-            {tb.map((item,index)=>(
+            {tb[lang].map((item,index)=>(
                         <Link key={index} to={item.to} className="list-group-item list-group-item-action">{item.title}</Link>
                 ))}
                 
@@ -29,7 +36,6 @@ export default class Info extends Component {
   
 </div>
 </>
-        
         )
-    }
 }
+export default Info;
