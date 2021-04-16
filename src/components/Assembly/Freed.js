0506973020842011720024981d18e5646bb6ae4f
Elvin Samanov <samanov.elvin96@mail.ru>
2021-04-16 10:00:29 +0400

    video for 2

--- src/components/Assembly/Freed.js
+++ src/components/Assembly/Freed.js
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next';
 const Freed=()=> {
     const { t, i18n } = useTranslation(['']);
     const lang=i18n.language;
-    let title=lang==="en"?"Discharge device":lang==="ru"?"Разрядное устройство":"Xaric olma qurğusu";
+    let title=lang==="en"?"Discharge device":lang==="ru"?"Выхлопное устройство":"Xaric olma qurğusu";
     let adres=lang==="en"?"/info/assembly":lang==="ru"?"/info/ustanovkaosnovnixcastey":"/info/esashisselerinqurgusu";
     let info2=lang==="en"?"Installation of main parts of TV3-117ВМ engine":lang==="ru"?"Установка основных частей двигателя ТВ3-117ВМ":"ТВ3-117ВМ müherrikin əsas hissələrin qurğusu";
     let info1=lang==="en"?"TB3-117BM Engine Subtopics":lang==="ru"?"Подтемы двигателя TB3-117M":"TB3-117BM mühərrikinə aid altmövzular";
@@ -20,6 +20,9 @@ const Freed=()=> {
                     <BreadcrumbItem name={info2} href={adres} />
                     <BreadcrumbItem name={title} current />
                 </Breadcrumb>
+                <div className="forVideo" style={{textAlign:"center",width:"100%",height:"75vh",marginBottom:"10px"}}>
+                <iframe width="70%" height="100%" src="https://www.youtube.com/embed/DTUGk1cb7-8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
             </div>
         )
 }
