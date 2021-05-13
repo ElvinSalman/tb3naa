31371a309603497147db5d56d4e0b1244b92543f
Elvin Samanov <samanov.elvin96@mail.ru>
2021-05-13 12:39:06 +0400

    delete type of links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -36,21 +36,7 @@ import { useTranslation } from 'react-i18next';
 {t('adres')}
 </span>
 </div>
-
 <div style={{marginTop:50,marginBottom:50,position:"relative",height:400}} className="col-12 col-md-12 map text-center">
-    {/* <h2>Saytı yazan tələbə haqqında məlumat</h2> */}
-
-    {/* <Map google={this.props.google}
-          initialCenter={{
-            lat: 40.45331,
-            lng: 50.06615
-          }}
-          zoom={13}
-          onClick={this.onMapClicked}>
- 
- <Marker onClick={this.onMarkerClick}
-         name={'Current location'} />
-</Map> */}
 <iframe
               className="contact-map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.9971856127218!2d50.06531561489584!3d40.453199261345524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4030608b9a760659%3A0x782f42f85f9e39ef!2sNational%20Aviation%20Academy!5e0!3m2!1sru!2s!4v1609582023179!5m2!1sru!2s"
@@ -60,21 +46,9 @@ import { useTranslation } from 'react-i18next';
               allowFullScreen
             />
 </div>
-        {/* <div className="col-12 col-md-6">
-            
-        </div>
-        <div className="col-12 col-md-6">
-            
-        </div> */}
 </div>
-     
 </div>
 
-{/* <div className="container">
-    <div className="row">
-    
-    </div>
-</div>  */}
 </>
         
         )
@@ -82,7 +56,3 @@ import { useTranslation } from 'react-i18next';
 }
 
 export default Contact;
-
-// export default GoogleApiWrapper({
-//     apiKey: ("AIzaSyDkg_nYYS4xon_dgwADolYvtVSWaU6fNTw")
-//   })(Contact)
